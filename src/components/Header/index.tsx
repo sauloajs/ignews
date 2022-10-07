c34d1fa6fb7489b62c4a0b872d621534a8b5cf24
Saulo Silva <saulo.silva@smartload.com.br>
2022-10-06 21:18:55 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignButton } from "../SignButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a href="" className={styles.active}>Home</a>
+          <a href="">Posts</a>
+        </nav>
+        <SignButton />
+      </div>
+    </header>
+  )
+}
