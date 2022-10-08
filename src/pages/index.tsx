f66ef00a522ff7a872d9df698325d0d73912fb36
Saulo Silva <saulo.silva@smartload.com.br>
2022-10-07 23:23:03 -0300

    SignBtn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,6 +8,20 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>
+              for $9.90
+            </span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
