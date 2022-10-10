f0be958a9a90c7e1698a55699965d7105d0f33bd
Saulo Silva <saulo.silva@smartload.com.br>
2022-10-10 20:35:47 -0300

    add faunadb

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -8,8 +8,6 @@ import styles from "./styles.module.scss";
 export function SignButton() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { client } from './../../../services/fauna';
+import { query } from "faunadb";
 
 export const authOptions = {
   providers: [
@@ -9,9 +11,44 @@ export const authOptions = {
     }),
   ],
   events: {
-    async signIn(message) { console.log(message) },
-    async signOut(message) { console.log(message) },
-    async session(message) { console.log(message) },
+    async signIn({ user, account, profile }) { 
+      try {
+        const { email, name, image } = user;
+        await client.query(
+          query.If(
+            query.Not(
+              query.Match(
+                query.Index('user_by_email'), 
+                query.Casefold(email)
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              {
+                data: {
+                  name, 
+                  email,
+                  image
+                }
+              }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_name'),
+                query.Casefold(email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+      
+    },
+    async signOut(message) {  },
+    async session(message) {  },
   }
 }
 
